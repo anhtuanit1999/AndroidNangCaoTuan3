5a626d1f1106a0076cc333a224ee9d3ff4033db9
AnhTuanIT <ngocdungbb1@gmail.com>
2021-09-17 17:01:14 +0700

    done

--- app/src/main/java/com/example/baitaptuan3/Screen3Activity.java
+++ app/src/main/java/com/example/baitaptuan3/Screen3Activity.java
@@ -3,12 +3,29 @@ package com.example.baitaptuan3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Screen3Activity extends AppCompatActivity {
-
+    private ImageView img_taylor;
+    private ImageView img_music;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen3);
+
+        img_taylor = findViewById(R.id.imageView2);
+        img_music = findViewById(R.id.imageView13);
+        Animation animation =
+                AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.anirotate);
+
+        Animation animation1 =
+                AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.anirotate);
+
+        img_taylor.startAnimation(animation);
+        img_music.startAnimation(animation1);
     }
 }
